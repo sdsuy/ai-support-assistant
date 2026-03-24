780f158019e2f3000f7a6e3fada41d44b878e020
sdsuy <santiagodasilva@sdsuy.dev>
2026-03-24 17:50:38 -0300

    chore: generate base modules (chat, ai)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChatModule } from './chat/chat.module';
+import { AiModule } from './ai/ai.module';
 
 @Module({
-  imports: [],
+  imports: [ChatModule, AiModule],
   controllers: [AppController],
   providers: [AppService],
 })
